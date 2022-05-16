84aa4112be24237416547e904b0cb7603289b858
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-05-16 15:22:42 +0200

    Configuring the model with attributes

--- MyBoardsMinimalAPI/Entities/WorkItem.cs
+++ MyBoardsMinimalAPI/Entities/WorkItem.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +12,33 @@ namespace MyBoardsMinimalAPI.Entities
     public class WorkItem
     {
         public int Id { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Column(TypeName = "varchar(200)")]
         public string Area { get; set; }
+
+        [Column("Iteration_Path")]
         public string IterationPath { get; set; }
+
         public int Priority { get; set; }
+
         //Epic
         public DateTime? StartDate { get; set; }
+
+        [Precision(3)]
         public DateTime? EndDate { get; set; }
+
         //Issue
+        [Column(TypeName = "decimal(5,2)")]
         public decimal Efford { get; set; }
+
         //Task
+        [MaxLength(200)]
         public string Activity { get; set; }
+
+        [Precision(14, 2)]
         public decimal RemaningWork { get; set; }
 
         public string Type { get; set; }
